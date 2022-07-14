37c213fc6c3d9f30f5e8b14f1ba4d48ae2820bc6
Eripron <87156859+Eripron@users.noreply.github.com>
2022-07-14 19:46:12 +0900

    today
    이미지 넣기 가격 이름 추가 이벤트시스템

--- Class/Assets/Script/Lifecy.cs
+++ Class/Assets/Script/Lifecy.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 //초기화 함수들의 공통 ( 게임이 실행될때 단 한번만 호출 됩니다.)
 //awake() 게임 오브젝트 비활성화도었을때도 실행되는함수. 
@@ -10,12 +11,41 @@ using UnityEngine;
 //물리 연산 
 //프레임 형태로 충돌하게 되면 불규칙하게 충돌 됩니다.
 //Fip Update  : Timestep 의 설정된 값에 따라 일정한 간격으로 충돌을 처리하기 위한 업데이트 함수.
+
+//유니티에서는 소멸자가 존재하지 않습니다.
+[System.Serializable]
+public class Data
+{
+    //변수
+    //기본 접근 지정자는 private 
+    //기본 생성자를 선언하지 않으면 자동으로 기본 생성자를 생성시켜줍니다.
+    public int price;
+    public Sprite sprite;
+    public string name;
+}
+
+
+
+
 public class Lifecy : MonoBehaviour
 {
-    int health = 0;
+    public int [] health;// 배열의 크기는 (?)
+    
+    public Data [] Data;
+
+    public Text[] priceUI;
+    public Text[] nameUI;
+    public Image[] spriteUI;
+
+    Stack<int>stack=new Stack<int>();
     private void Awake()
     {
-        Debug.Log("Tiger");
+        for(int i=0;i<Data.Length;i++)
+        {
+            priceUI[i].text = Data[i].price.ToString();
+            nameUI[i].text = Data[i].name;
+            spriteUI[i].sprite = Data[i].sprite;
+        }
     }
 
 
@@ -32,14 +62,22 @@ public class Lifecy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        health++;
-        Debug.Log(health);
-        if(health>=100)
-       
-                health=0;
+   
     }
+
     private void FixedUpdate()
     {
         Debug.Log("Tiger Fixed Update");
+    }
+
+    private void OnDisable()
+    {
+        // 게임 오브젝트가 비활성화 될때마다 호출되는 함수입니다.
+        Debug.Log("Tiger OnDisable");
+    }
+    private void OnDestroy()
+    {
+        //게임 오브젝트가 파괴되는 순간 호출되는 함수입니다.
+
     }
 }
