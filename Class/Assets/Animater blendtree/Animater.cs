397eef8d73b06371755cfd87f60f5b4662ce644f
Eripron <87156859+Eripron@users.noreply.github.com>
2022-07-18 19:46:33 +0900

    animation
    animation

---
+++ Class/Assets/Animater blendtree/Animater.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+//백터 :크기랑 방향을 가집니다.
+//스칼라 : 크기만 가지고 있는 물리량
+public class Animater : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
